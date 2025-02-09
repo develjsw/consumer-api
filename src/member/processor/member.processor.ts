a2b005c8bae8348b4a337831fe90f9b788448b30
jsw <develjsw@gmail.com>
2025-02-09 16:12:40 +0900

    consumer API 로그 수정

--- src/member/processor/member.processor.ts
+++ src/member/processor/member.processor.ts
@@ -11,31 +11,33 @@ export class MemberProcessor {
     @Process('createMemberJob')
     async createMember(job: Job): Promise<void> {
         console.log(job.data);
-        const { data } = job;
+        try {
+            await this.createMemberService.createMember(job.data);
+        } catch (error: any) {
+            console.error(`[ 회원 생성 실패 ] - JobId : ${job.id}, Error: ${error.message}`);
 
-        if (data) {
-            try {
-                await this.createMemberService.createMember(data);
-            } catch (error: any) {
-                console.error(`회원생성 큐 데이터 적재 실패 : ${error}`);
+            // 재시도 횟수 3회 초과시 Slack 알림 발송
+            if (job.attemptsMade >= 3) {
+                // Slack 알림 발송 로직..
+                console.error(`[ 회원 생성 실패 - 재시도 3회 초과 ] - JobId : ${job.id}`);
             }
         }
     }
 
     @OnQueueActive()
-    async onActive(job: Job) {
-        console.log(`[ 수행완료 ] - [ name : ${job.name} id : ${job.id} ]`);
+    async onActive(job: Job): Promise<void> {
+        console.log(`[ 작업 시작 ] - JobName : ${job.name} JobId : ${job.id}`);
     }
 
     /*
         [ Job이 Queue에 추가됬지만, 아직 실행되지 않았을 때 실행됨 ]
-        - delay 옵션을 설정하여 추가된 작업건으로 아직 실행되지 않은 경우
-        - 실행 중인 Worker가 부족하여 작업이 대기열(Wating)에 들어간 경우
-        - Paused 상태에서 작업이 대기중인 경우
-        - 특정 Job이 먼저 실행되도록 priority 설정을 한 경우
+        - Case1. delay 옵션을 설정하여 추가된 작업건으로 아직 실행되지 않은 경우
+        - Case2. 실행 중인 Worker가 부족하여 작업이 대기열(Wating)에 들어간 경우
+        - Case3. Paused 상태에서 작업이 대기중인 경우
+        - Case4. 특정 Job이 먼저 실행되도록 priority 설정을 한 경우
     */
     @OnQueueWaiting()
-    async onWaiting(jobId: number) {
-        console.log(`작업 ${jobId}가 대기 큐에 들어갔음`);
+    async onWaiting(jobId: number): Promise<void> {
+        console.log(`[ 작업 대기 ] - JobId ${jobId}가 대기 큐에 들어감`);
     }
 }
